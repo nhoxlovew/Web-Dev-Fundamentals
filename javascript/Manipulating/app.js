b9942535c8fbd0bfade66f242cd1403c40c3dd9b
Hoàng Anh <hoanganhonzed@gmail.com>
2025-06-11 22:42:07 +0700

    Implement movie list and basic filtering functionality; add HTML structure for DOM manipulation and styling

---
+++ javascript/Manipulating/app.js
@@ -0,0 +1,8 @@
+// const allImgaes = document.getElementsByTagName("img");
+const allImgaes = document.querySelectorAll(".square:nth-of-type(2)");
+
+for (let img of allImgaes) {
+    img.src = "https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80"
+}
+
+
--- javascript/callbacks/js.js
+++ javascript/callbacks/js.js
@@ -1,40 +1,40 @@
-// const movies = [
-//     {
-//         title: "Inception",
-//         director: "Christopher Nolan",
-//         year: 2010,
-//         rating: 8.8,
-//         score: 90
-//     },
-//     {
-//         title: "The Matrix",
-//         director: "The Wachowskis",
-//         year: 1999,
-//         rating: 8.7,
-//         score: 90
-//     },
-//     {
-//         title: "Interstellar",
-//         director: "Christopher Nolan",
-//         year: 2014,
-//         rating: 8.6,
-//         score: 89
-//     },
-//     {
-//         title: "The Dark Knight",
-//         director: "Christopher Nolan",
-//         year: 2008,
-//         rating: 9.0,
-//         score: 91
-//     },
-//     {
-//         title: "Pulp Fiction",
-//         director: "Quentin Tarantino",
-//         year: 1994,
-//         rating: 8.9,
-//         score: 88
-//     }
-// ]
+const movies = [
+    {
+        title: "Inception",
+        director: "Christopher Nolan",
+        year: 2010,
+        rating: 8.8,
+        score: 90
+    },
+    {
+        title: "The Matrix",
+        director: "The Wachowskis",
+        year: 1999,
+        rating: 8.7,
+        score: 90
+    },
+    {
+        title: "Interstellar",
+        director: "Christopher Nolan",
+        year: 2014,
+        rating: 8.6,
+        score: 89
+    },
+    {
+        title: "The Dark Knight",
+        director: "Christopher Nolan",
+        year: 2008,
+        rating: 9.0,
+        score: 91
+    },
+    {
+        title: "Pulp Fiction",
+        director: "Quentin Tarantino",
+        year: 1994,
+        rating: 8.9,
+        score: 88
+    }
+]
 
 
 // const recentMovies = movies.filter(movie =>{
@@ -43,14 +43,20 @@
 
 
 
-function validUserNames(usernames) {
-    // your code here
-   return usernames.filter(newuser => { return newuser.length < 10 })
-    
-}
+// function validUserNames(usernames) {
+//     // your code here
+//    return usernames.filter(newuser => { return newuser.length < 10 })
+
+// }
 
 
 //   const filterUser =
 // ['mark', 'staceysmom1978', 'q29832128238983', 'carrie98', 'MoanaFan']
+
+
+function newMoiveListScore(numb) {
+    const Newlist = movies.filter(m => m.score > numb)
+    console.log(Newlist);
+}
 
 
